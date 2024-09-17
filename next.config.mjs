144eca980ffa5445ac6525abd7411d3180b99724
Kattsuuya <angelattack9759@gmail.com>
2024-09-18 00:10:53 +0900

    chore: プロファイル機能を導入
    - [公式ドキュメント](https://nextjs.org/docs/app/building-your-application/configuring/environment-variables)によるとデフォルトでは環境プロファイルはproduction, development, testの3つしか扱えないため、任意のプロファイルを追加できるように処理を追加した
      - 下記の記事を参考にさせてもらった
        - https://zenn.dev/aktriver/articles/2022-04-nextjs-env
    - macOS/LinuxとWindowsでは環境変数の指定方法が異なるため、cross-envというライブラリを導入してWindowsでもUnixスタイルで指定できるようにした

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,27 @@
+import fs from "node:fs";
+import path from "node:path";
+
 /** @type {import('next').NextConfig} */
 const nextConfig = {};
+
+/**
+ * 環境変数を読み込む
+ *
+ * @param {string} profile プロファイル名
+ */
+const loadEnv = (profile) => {
+  const envFilePath = path.resolve(`./env/env.${profile}.json`);
+  const env = {
+    ...JSON.parse(fs.readFileSync(envFilePath, "utf-8")),
+    NEXT_PUBLIC_PROFILE: profile,
+  };
+
+  for (const [key, value] of Object.entries(env)) {
+    process.env[key] = value;
+  }
+};
+
+// デフォルトでは開発環境上での動作を想定している
+loadEnv(process.env.PROFILE || "dev");
 
 export default nextConfig;
